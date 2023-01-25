a20f6d9a1d8288c4efbcd6cb78c3326581daa916
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-01-25 17:52:20 -0300

    Feedback form

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -37,6 +37,16 @@ export function Post(props: any) {
           <a href="#"> #rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
